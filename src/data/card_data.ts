c98253a1c55fc423a214d560b90862f7089818ec
Mateusz Rojek <Merkius@wp.pl>
2024-07-05 14:19:52 +0200

    card fix

--- src/data/card_data.ts
+++ src/data/card_data.ts
@@ -1,36 +1,36 @@
 const card_data = [
   {
-    imageUrl: "../../../public/assets/img/thumb/lol.jpg",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/lol.jpg`,
     gameName: "League of legends",
     url: "/calculator/lol",
     done: true,
   },
   {
-    imageUrl: "../../../public/assets/img/thumb/coaching.png",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/coaching.png`,
     gameName: "Coaching",
     url: "/coaching",
     done: true,
   },
   {
-    imageUrl: "../../../public/assets/img/thumb/tft.png",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/tft.png`,
     gameName: "Teamfight Tactics",
     url: "/",
     done: false,
   },
   {
-    imageUrl: "../../../public/assets/img/thumb/dota.png",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/dota.png`,
     gameName: "Dota 2",
     url: "/",
     done: false,
   },
   {
-    imageUrl: "../../../public/assets/img/thumb/valorant.png",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/valorant.png`,
     gameName: "Valorant",
     url: "/",
     done: false,
   },
   {
-    imageUrl: "../../../public/assets/img/thumb/cs2.png",
+    imageUrl: `${process.env.PUBLIC_URL}/assets/img/thumb/cs2.png`,
     gameName: "CS2",
     url: "/",
     done: false,
