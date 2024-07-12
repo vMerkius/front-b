9000f59d9c7a2ae15287cff65320f205e42d2a1e
Mateusz Rojek <Merkius@wp.pl>
2024-07-12 02:02:00 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,15 @@ import "react-modal-video/scss/modal-video.scss";
 import "./index.scss";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+console.log("clientId", clientId);
 
-if (!clientId) {
-  throw new Error("REACT_APP_GOOGLE_CLIENT_ID is not defined");
-}
+// if (!clientId) {
+//   throw new Error("REACT_APP_GOOGLE_CLIENT_ID is not defined");
+// }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
+    <GoogleOAuthProvider clientId="306475290852-pu7mlaplgf5gubucs223kpjjp3ddvegs.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
